04e6178fbf7ef21ff5f99f8f015fef5ae73c5aa3
MarcioSousa <marciofrapeso@gmail.com>
2022-11-16 22:15:28 -0300

    Aparecendo qtde de Prateleiras nos quadrados.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,24 @@
+//Selecionada todas as células
 const cellElements = document.querySelectorAll("[data-cell]");
 
-//let isCircleturn = false;
-
 const handledClick = (e) => {
+    //Aparecer uma mensagem para digitar um valor, sendo 1, 2 ou 3
     const cell = e.target;
-    const classToAdd = "x";
+
+    //Colocando a, aparece 1;
+    //Colocando b, aparece 2;
+    //Colocanod c, aparece 3;
+    const classToAdd = "c";
+    
+    
     
     // Deixar o quadrado selecionado.
     cell.classList.add(classToAdd);
 };
 
+//Cria um for para todos as células para que fiquem ajeitadas e
+//prontas para serem clicadas.
 for (const cell of cellElements){
-    cell.addEventListener("click", handledClick, {once: true});
+    //once : false = poderá ser clicado mais de uma vez
+    cell.addEventListener("click", handledClick, {once: false});
 }
